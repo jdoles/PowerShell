5e8e61b39f1a6b6ccbfa782f29928c6f0b3363aa
Justin <jmdoles@gmail.com>
2025-05-19 20:04:15 -0400

    Updated to include the option to remove the apps from the image.

--- SysAdmin/RemoveUnwantedApps.ps1
+++ SysAdmin/RemoveUnwantedApps.ps1
@@ -2,70 +2,130 @@
     RemoveUnwantedApps.ps1
     Author: Justin Doles
     Requires: PowerShell 5 or higher
-    Updated: 2025-05-16
+    Updated: 2025-05-19
 #>
 <#
     .SYNOPSIS
         This script removes unwanted Windows apps from the system.
     .DESCRIPTION
         This script removes unwanted Windows apps from the system using PowerShell. It checks for installed apps with names matching specific patterns and attempts to uninstall them.
+        It also removes these apps from the system image if specified.
     .EXAMPLE
         RemoveUnwantedApps.ps1
+        RemoveUnwantedApps.ps1 -RemoveFromImage
     .NOTES
         This script requires PowerShell 5 or higher and administrative privileges.
-    .PARAMETER None
-        No parameters are required for this script.
+    .PARAMETER RemoveFromImage
+        If specified, the script will remove the apps from the system image as well.
 #>
 
-# List of unwanted Microsoft apps to remove
-$software = @(
-    "Microsoft.XboxGameOverlay",
-    "Microsoft.XboxGamingOverlay",
-    "Microsoft.Xbox.TCUI",
-    "Microsoft.XboxApp",
-    "Microsoft.XboxIdentityProvider",
-    #"Microsoft.XboxGameCallableUI", # this does not seem to be removable
-    "Microsoft.XboxSpeechToTextOverlay",
-    "Microsoft.MinecraftEducationEdition"
-    "Microsoft.YourPhone",
-    "Microsoft.SkypeApp"
+
+param (
+    [Parameter(Mandatory=$false)]
+    [switch]$RemoveFromImage
 )
 
-# Counter for the number of apps found
-$appsFound = 0
+function Write-Log {
+    param (
+        [string]$Message
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    Write-Host "[$timestamp] $Message"
+}
 
-# Loop through each unwanted app and remove it if found
-foreach ($app in $software) {
-    Write-Host "Checking for $app..."
+# Function to remove unwanted apps
+function Remove-Apps {
+    param (
+        [string]$AppName,
+        [switch]$RemoveFromImage
+    )
+
     # Get the app information using Get-AppxPackage
-    $appInfo = Get-AppxPackage -AllUsers | Where-Object {$_.name -like "*$app*"}
+    $appInfo = Get-AppxPackage -AllUsers | Where-Object {$_.name -like "*$AppName*"}
     if ($appInfo) {
         if ($appInfo.Count -gt 1) {
-            Write-Host "Multiple instances of $($appInfo.Name) found. Removing all instances."
+            Write-Log "[INFO] Multiple instances of $($appInfo.Name) found. Removing all instances."
             foreach ($instance in $appInfo) {
                 if ($instance.NonRemovable -eq $true) {
-                    Write-Host "Cannot remove: $($instance.Name) (Non-removable)"
+                    Write-Log "[WARN] Cannot remove: $($instance.Name) (Non-removable)"
                 } else {
-                    Write-Host "Removing: $($instance.Name)"
                     # Remove the app using Remove-AppxPackage
+                    Write-Log "[INFO] REMOVE: $($instance.Name)"
                     $instance | Remove-AppxPackage -AllUsers
-                    $appsFound++    
-                }                
+                    $script:appsRemoved++
+                }
             }
         } else {
-            Write-Host "Removing: $($appInfo.Name)"
+            Write-Log "[INFO] REMOVE: $($appInfo.Name)"
             # Remove the app using Remove-AppxPackage
             $appInfo | Remove-AppxPackage -AllUsers
-            $appsFound++
+            $script:appsRemoved++
         }
     } else {
-        Write-Host "$app not found."
+        Write-Log "[INFO] $appName not found."
+    }
+
+    # If the RemoveFromImage switch is specified, remove the app from the system image
+    if ($RemoveFromImage) {
+        $appImage = Get-AppxProvisionedPackage -Online | Where-Object {$_.DisplayName -like "*$AppName*"}
+        if ($appImage) {
+            # Remove the app from the image using Remove-AppxProvisionedPackage
+            Write-Log "[INFO] REMOVE: $AppName from image..."
+            $appImage | Remove-AppxProvisionedPackage -Online
+            $script:appsRemoved++
+        } else {
+            Write-Log "[INFO] $AppName not found in image."
+        }
     }
 }
 
+# List of unwanted Microsoft apps to remove
+$software = @(
+    ".HPSystemInformation",
+    ".HPPrivacySettings",
+    ".HPPCHardwareDiagnosticsWindows",
+    ".myHP",
+    "Microsoft.BingFinance",
+    "Microsoft.BingNews",
+    "Microsoft.BingSports",
+    "Microsoft.BingWeather",
+    "Microsoft.Getstarted"
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.MicrosoftOfficeHub",
+    "Microsoft.MinecraftEducationEdition",
+    "Microsoft.SkypeApp",
+    "Microsoft.WindowsCommunicationsApps",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsMaps",
+    "Microsoft.XboxApp",    
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxIdentityProvider",
+    #"Microsoft.XboxGameCallableUI", # this does not seem to be removable
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.YourPhone",
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo"
+)
+
+# Counter for the number of apps found
+$appsRemoved = 0
+
+# Loop through each unwanted app and remove it if found
+
+Write-Log "[INFO] $($software.Count) unwanted apps will be removed."
+foreach ($app in $software) {
+    Write-Log "[INFO] Checking for $app..."
+    if ($RemoveFromImage) {
+        Remove-Apps -AppName $app -RemoveFromImage
+    } else {
+        Remove-Apps -AppName $app
+    }    
+}
+
 # Check if any unwanted apps were found and removed
-if ($appsFound -eq 0) {
-    Write-Host "No unwanted apps found."
+if ($appsRemoved -eq 0) {
+    Write-Log "[INFO] No unwanted apps removed."
 } else {
-    Write-Host "$appsFound unwanted apps removed."
+    Write-Log "[INFO] $appsRemoved unwanted apps removed."
 }
